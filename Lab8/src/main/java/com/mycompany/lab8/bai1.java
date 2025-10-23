14e89d18a656197340117adebb4e260ca72109a0
HI <HI@192.168.1.3>
2025-10-24 00:05:51 +0700

    asm2

--- Lab8/src/main/java/com/mycompany/lab8/bai1.java
+++ Lab8/src/main/java/com/mycompany/lab8/bai1.java
@@ -11,6 +11,6 @@ package com.mycompany.lab8;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello khanh an");
     }
 }
